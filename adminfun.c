6b24fbc0cd834e38a1a98eff7fa1cb1ff75c6999
Anjan Budige Altruist <61613373+anjan-budige@users.noreply.github.com>
2023-03-12 10:07:25 +0530

    Add files via upload

---
+++ adminfun.c
@@ -0,0 +1,59 @@
+#include <string.h>
+#include <conio.h>
+static int ac = 14312;
+struct cust{
+  char name[20];
+  int accno;
+  int bal;
+  char pass[10];
+}cust1;
+void add_acc(){
+  clrscr();
+  char name[20];
+  char name2[20];
+  char cust_name[20];
+  printf("----Add Account-----\n\n");
+  printf("Enter Customer Name: ");
+  scanf("%s",cust1.name);
+  strcpy(name,cust1.name);
+  strcpy(cust_name,name);
+  cust1.accno = ++ac;
+  printf("Enter Customer Opening Balance: ");
+  scanf("%d",&cust1.bal);
+  printf("Enter Customer Password: ");
+  scanf("%s",cust1.pass);
+  char temp[20] = "cust_acc/";
+  strcat(temp,name);
+  char temp2[20] = ".txt";
+  strcat(temp,temp2);
+  FILE *fp;
+  fp = fopen(temp,"w+");
+  fputs(cust1.pass,fp);
+  fclose(fp);
+  char temp3[20] = "cust_details/";
+  char temp4[10] = ".txt";
+  strcpy(name2,cust1.name);
+  strcat(temp3,name2);
+  strcat(temp3,temp4);
+  printf("%s",temp3);
+  
+  /*fp = fopen(temp3, "w+");
+  fprintf(fp,"---Account Details---\n\n");
+  fprintf(fp,"Name : %s\nAccount No: %d\nBalance: %d\n",cust_name,cust1.accno,cust1.bal);
+  
+  fclose(fp);
+  fp = fopen(temp3, "r");
+  if(fp){
+    printf("Successful Added Account\n");
+    char c;
+    c = fgetc(fp);
+    while(c!=EOF){
+      printf(" %c",c);
+      c = fgetc(fp);
+    }
+  }
+    else{
+      printf("Something Went Wrong");
+    }*/
+  }
+  
---
+++ mode.c
@@ -0,0 +1,128 @@
+#include <string.h>
+#include <conio.h>
+#include "adminfun.c"
+
+void admin(){
+  clrscr();
+  char user[20];
+  char pass[20];
+  char pass2[20];
+  int flag = 0;
+	printf("\n--- Admin Login ----\n");
+	printf("Enter Username: ");
+	scanf("%s",user);
+	char file[10] = ".txt";
+	strcat(user,file);
+	
+	FILE *fp;
+	fp = fopen(user, "r");
+	if(fp != NULL){
+	  printf("Enter Password: ");
+	  scanf("%s",pass);
+	  fgets(pass2,10,fp);
+	  if(strcmp(pass2,pass)==0){
+	    printf("Login Successful\n");
+	    flag =1;
+	    fclose(fp);
+	  }
+	  else{
+	    printf("Password Wrong Please Check It\n");
+	  }
+	  
+	  
+	  
+	  
+	}
+	else{
+	  printf("User Not Found");
+	}
+	
+	
+	if(flag==1){
+	printf("1. Add Account\n2. Delete Account \n3. Check All Account\n4. Edit Account\n5. Exit\n");
+	
+	int a;
+	printf("Enter Your Option: ");
+	scanf("%d",&a);
+	switch(a){
+	  case 1:{
+	    add_acc();
+	    break;
+	  }
+	  case 2:{
+	    //delete_acc();
+	    break;
+	  }
+	  case 3:{
+	    //check_all();
+	    break;
+	  }
+	  case 4:
+	  {
+	    //edit_acc();
+	    break;
+	  }
+	  case 5:{
+	    //exit(0);
+	    
+	  }
+	  default:{
+	    printf("Invalid Option\n");
+	  }
+	 
+	  
+	}
+	
+	
+	}
+	
+
+	
+	
+}
+void staff(){
+  char user[20];
+  char pass[20];
+  char pass2[20];
+  int flag = 0;
+	printf("\n--- Staff Login ----\n");
+	printf("Enter Username: ");
+	scanf("%s",user);
+	char file[20] = "staff_acc/";
+	strcat(file,user);
+	char file2[20] = ".txt";
+	strcat(file,file2);
+	char filen[20];
+	strcpy(filen,file);
+	FILE *fp;
+	fp = fopen(filen, "r");
+	if(fp != NULL){
+	  printf("Enter Password: ");
+	  scanf("%s",pass);
+	  fgets(pass2,10,fp);
+	  if(strcmp(pass2,pass)==0){
+	    printf("Login Successful\n");
+	    flag =1;
+	    fclose(fp);
+	  }
+	  else{
+	    printf("Password Wrong Please Check It\n");
+	  }
+	  
+	  
+	  
+	  
+	}
+	else{
+	  printf("User Not Found");
+	}
+	
+	
+	if(flag==1){
+	printf("1. Withdraw\n2. Deposit \n3. User Balance Check\n4. Edit Account\n5. Exit\n");
+	}
+	
+}
+void customer(){
+	printf("--- Customer Login----");
+}
